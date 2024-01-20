5120145cd37028df83f1753a3f17cc1968ad78bc
SalmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-20 23:27:03 +0530

    create project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {HomePageComponent} from "./pages/home/home-page/home-page.component";
+import {ProductPageComponent} from "./pages/product/product-page/product-page.component";
+import {NotFoundPageComponent} from "./core/not-found-page/not-found-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+  {path:'home',component:HomePageComponent},
+  {path:'products',component:ProductPageComponent},
+  {path:'**',component:NotFoundPageComponent},
+];
